a29c4e33a7f958e942d81c01b8706751a407348e
vivek jain <vivek.jain@mountblue.io>
2018-10-10 18:59:40 +0530

    Fixed date issue

---
+++ app/renderer/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.querySelector(document.currentScript.getAttribute('data-container'));
+
+ReactDOM.render(
+    <div>Inital commit</div>,
+  rootElement,
+);
