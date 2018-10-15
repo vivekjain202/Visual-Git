2ebf04bca7235ce9f29337dff78e5b0eee533042
Ajay Kumar <ajay.kumar@mountblue.io>
2018-10-15 15:02:49 +0530

    remove CurrentRrepo and current branch reducers and add in global.js

--- app/renderer/reducers/global.js
+++ app/renderer/reducers/global.js
@@ -1,19 +1,21 @@
-import {CHANGE_BRANCH, CHANGE_BRANCH_COMMITS} from '../constants/actions'
+import { CHANGE_BRANCH, CHANGE_BRANCH_COMMITS, CHANGE_REPOSITORY_BRANCHES } from '../constants/actions'
 const initialState = {
     currentRepo: 'VisualGit',
     currentBranch: 'Master',
-    branches: [{name:'Master', selected:false}, {name:'experiment', selected: false}],
+    branches: [{ name: 'Master', selected: false }, { name: 'experiment', selected: false }],
     currentBranchCommits: [],
     otherBranches: []
 };
 export default (state = initialState, action) => {
-    switch(action.type){
+    switch (action.type) {
         case CHANGE_BRANCH:
             state = { ...state, currentBranch: action.payload, branches: updateBranches(state.branches, action.payload) }
             break;
         case CHANGE_BRANCH_COMMITS:
             state = { ...state, currentBranchCommits: getCurrentBranchCommits(state.branches, action.payload) }
             break;
+        case CHANGE_REPOSITORY_BRANCHES:
+            state = { ...state, branches: getRepositoryBranches(action.payload, state) }
     }
 
     return state;
@@ -31,4 +33,8 @@ const getCurrentBranchCommits = (branches, branchName) => {
         if (branch.name === branchName) return branch.commits
     })
     return commits
+}
+
+const getRepositoryBranches =() => {
+
 }
