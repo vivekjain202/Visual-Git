f9a518c2a38904dfa5c8fb711c484cafe7dca114
vivek jain <vivek.jain@mountblue.io>
2018-10-15 22:56:02 +0530

    build configuration fixed,build icon fixed

--- app/main/index.js
+++ app/main/index.js
@@ -70,9 +70,10 @@ app.on('ready', async () => {
     minWidth: 640,
     minHeight: 480,
     show: false,
-    icon:image,
+    // icon:image,
   });
   mainWindow.maximize();
+  mainWindow.setTitle='Visual Git'
   const template=[
     {
       label:'File',
