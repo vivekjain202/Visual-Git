54c004871649c6cf5e4344194e205cb3bdc127e2
Ajay Kumar <ajay.kumar@mountblue.io>
2018-10-16 11:31:40 +0530

    fix store import in app.js

--- app/renderer/app.js
+++ app/renderer/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './store/index';
 
 const rootElement = document.querySelector(document.currentScript.getAttribute('data-container'));
 
