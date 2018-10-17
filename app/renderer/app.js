bf852e40f668721849a133611277e2bf5ce261d4
Shashank G Upadhya <shashank.upadhya@mountblue.io>
2018-10-17 10:19:06 +0530

    Added functions to handle menu item click in renderer

--- app/renderer/app.js
+++ app/renderer/app.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './store/index';
+import { ipcRenderer } from 'electron';
+import path from 'path';
+import {gitInit, cloneRepo, deleteRepo, createNewBranch, gitBranch, switchBranch, deleteBranch, gitLog, renameBranch} from './renderer-menu-functions.js';
+
+ipcRenderer.on('git-init-appmenu', gitInit);
+ipcRenderer.on('clone-repo-appmenu',cloneRepo);
+ipcRenderer.on('git-repo-delete-appmenu',deleteRepo);
+ipcRenderer.on('git-new-branch-appmenu',() => createNewBranch(path.join(__dirname,'../../../../trello-todo-shashank')));
+ipcRenderer.on('git-switch-branch-appmenu',() => switchBranch(path.join(__dirname,'../../../../trello-todo-shashank'),'temp'));
+ipcRenderer.on('git-delete-branch-appmenu',() => deleteBranch(path.join(__dirname,'../../../../trello-todo-shashank'),'newName'));
+ipcRenderer.on('git-rename-branch-appmenu',() => renameBranch(path.join(__dirname,'../../../../trello-todo-shashank'),'temp','newName'));
+
+
 
 const rootElement = document.querySelector(document.currentScript.getAttribute('data-container'));
 
