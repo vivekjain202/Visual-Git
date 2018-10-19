7b11a4e5c7a5aacccb1d635a26a35b4c248ad6f9
Ajay Kumar <ajay.kumar@mountblue.io>
2018-10-19 13:42:04 +0530

    fix opening the electron dialog from Home.js to react dialog

--- app/renderer/app.js
+++ app/renderer/app.js
@@ -8,7 +8,7 @@ import path from 'path';
 
 import {gitInit, cloneRepo, deleteRepo, deleteBranch, renameBranch} from '../renderer/components/RepoHome/SelectionBar/renderer-menu-functions.js';
 
-ipcRenderer.on('git-init-appmenu', gitInit);
+// ipcRenderer.on('git-init-appmenu', gitInit);
 ipcRenderer.on('git-repo-delete-appmenu',deleteRepo);
 ipcRenderer.on('git-delete-branch-appmenu',() => deleteBranch(path.join(__dirname,'../../../../trello-todo-shashank'),'newName'));
 ipcRenderer.on('git-rename-branch-appmenu',() => renameBranch(path.join(__dirname,'../../../../trello-todo-shashank'),'temp','newName'));
